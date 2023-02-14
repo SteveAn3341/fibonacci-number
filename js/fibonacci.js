ef0a3cbc03c58fab82442af2464a32db79e96c06
YOUR_NAME <steve3341@DESKTOP-DEMPQ84>
2023-02-14 18:22:49 -0500

    fibonacci

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,25 @@
 const fibonacci = (num) => {
+    let cycle = num
+    let a = 0
+    let y = 1
+    let z = 1
+
+      i = 2
+      while(cycle > i){
+        a = y
+        y = z
+        z = a + y
+        i++
+        console.log(a,y)
+      }
+      return z
+   
     
 }
 
-module.exports = {fibonacci}
+console.log(fibonacci(11))
+
+
+
+
+  
