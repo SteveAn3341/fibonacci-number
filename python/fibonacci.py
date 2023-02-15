be96c7e9849f1ca40d51c631a4472e93c574f3b6
SteveAn3341 <107530990+SteveAn3341@users.noreply.github.com>
2023-02-14 19:08:28 -0500

    Add files via upload

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,29 @@
 def fibonacci(n):
-  pass
+  a = 0
+  b = 1
+  c = a + b
+  cycle = 2
+  while n > cycle:
+    a = b
+    b = c
+    c = a + b
+    cycle += 1
+  return c
 
+print(fibonacci(8))
 
+# def fibonacci(n):
+#   if n <= 0:
+#     return 0
+#   elif n == 1:
+#     return 1
+#   else:
+#     a = 0
+#     b = 1
+#     for i in range(2, n+1):
+#       c = a + b
+#       a = b
+#       b = c
+#     return c
+
+# print(fibonacci(8))
